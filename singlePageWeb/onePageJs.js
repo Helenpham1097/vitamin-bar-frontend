a0cc9788b207288a19dba4452b7da80f7ef024f0
Helenpham1097 <helenpham1097@gmail.com>
2022-02-05 15:27:02 +1300

    add smooth croll

--- singlePageWeb/onePageJs.js
+++ singlePageWeb/onePageJs.js
@@ -2,7 +2,7 @@
 /* jshint browser: true */
 /*jshint esversion: 6 */
 /*jshint sub:true*/
-
+/*globals $:false */
 
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
@@ -34,3 +34,20 @@ menu_item.forEach((item) => {
         mobile_menu.classList.toggle('active');
     });
 });
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if(this.hash !==""){
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().left
+            }, 300, function () {
+                window.location.hash=hash;
+            });
+        }
+    });
+});
+
+
+
