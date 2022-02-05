62e0c4c77757c7fb5794d975676fc1a46f41e182
Helenpham1097 <helenpham1097@gmail.com>
2022-02-05 14:26:45 +1300

    single page web design demo

---
+++ singlePageWeb/onePageJs.js
@@ -0,0 +1,36 @@
+"use strict";
+/* jshint browser: true */
+/*jshint esversion: 6 */
+/*jshint sub:true*/
+
+
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header .container');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+
+hamburger.addEventListener('click',()=> {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', ()=>{
+    let scroll_position = window.scrollY;
+    if(scroll_position>250){
+        header.style.backgroundColor ='#29323c';
+    }
+    else {
+        header.style.backgroundColor ='transparent';
+    }
+});
+
+// document.addEventListener('click',()=>{
+//     mobile_menu.animate()
+// })
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
